b8a1f18a97c6c1e45750717764a2faa5388adcc3
Myrxmy <1655895179@qq.com>
2026-05-13 01:50:46 +0800

    AI 应用开发 1.接入LangChain4j，实现 AI 对话服务 2.开发文件保存器和代码解析器、AI 生成门面，实现 AI 生成网站文件 3.SSE 流式输出和解析

---
+++ src/test/java/com/myr/aicode/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,27 @@
+package com.myr.aicode.ai;
+
+import com.myr.aicode.ai.model.HtmlCodeResult;
+import com.myr.aicode.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个程序员的工作记录小工具,不超过20行");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员的留言板，不超过50行");
+        Assertions.assertNotNull(multiFileCode);
+    }
+}
