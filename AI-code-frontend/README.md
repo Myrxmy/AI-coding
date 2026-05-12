e93ea88d17e3192652e46dd551ac2658e43d9eb6
Myrxmy <1655895179@qq.com>
2026-05-12 22:28:15 +0800

    用户模块前后端代码

--- AI-code-frontend/README.md
+++ AI-code-frontend/README.md
@@ -1,4 +1,4 @@
-# AI-code-frontend
+# yu-ai-code-mother-frontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/main/java/com/myr/aicode/AiCodeApplication.java
+++ src/main/java/com/myr/aicode/AiCodeApplication.java
@@ -1,9 +1,11 @@
 package com.myr.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.myr.aicode.mapper")
 public class AiCodeApplication {
 
     public static void main(String[] args) {
