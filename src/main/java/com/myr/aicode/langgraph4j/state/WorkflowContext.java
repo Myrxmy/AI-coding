5b696deeeff18dd3c35fbae2e8d60c3b72260e81
Myrxmy <1655895179@qq.com>
2026-06-01 16:37:59 +0800

    AI 工作流（LangGraph4j 高级特性实战 - 图片并发收集） 1. 基于 LangGraph4j Parallel Branch 实现并发

--- src/main/java/com/myr/aicode/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/myr/aicode/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.myr.aicode.langgraph4j.state;
 
+import com.myr.aicode.langgraph4j.model.ImageCollectionPlan;
 import com.myr.aicode.langgraph4j.model.ImageResource;
 import com.myr.aicode.langgraph4j.model.QualityResult;
 import com.myr.aicode.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,19 @@ public class WorkflowContext implements Serializable {
      * 错误信息
      */
     private String errorMessage;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
     @Serial
     private static final long serialVersionUID = 1L;
